3b958856810f1ba1df0bcd4ab0822ec868860f5f
GemsGame <a.masanovets@gmail.com>
2020-05-19 18:38:24 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*fetch('https://swapi.dev/api/people/1/')
+.then(response => response.json())
+.then(result => console.log(result))
+.catch(error => console.log(error));
+*/
+class SwapiService {
+  _apiHost = 'https://swapi.dev/api';
+  async getResourse(url) {
+    const response = await fetch(`${this._apiHost}${url}`);
+    if (!response.ok) {
+      throw new Error(`Could not fetch ${response.status}`)
+    } 
+    return await response.json();
+  }
+  
+  async getAllPeople () {
+    const result = await this.getResourse('/people/');
+    return result.results;
+  }
+  async getPerson (id) {
+   return await this.getResourse(`/people/${id}`);
+  }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  async getAllStarships () {
+   const result = await this.getResourse('/starships/');
+   return result.results;
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  async getStarship(id) {
+    return await this.getResourse(`/starships${id}`);
+  }
+
+  async getAllPlanets () {
+    const result = await this.getResourse('/planets/');
+    return result.results;
+   }
+
+   async getPlanet (id) {
+    const result = await this.getResourse(`/planets/${id}`);
+    return result;
+   }
+}
+
+const swapi = new SwapiService();
+swapi.getPlanet(3).then(result => console.log(result));
