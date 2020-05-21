fcf7428ce7cff1f560a44cfac8bb829392384f3c
GemsGame <a.masanovets@gmail.com>
2020-05-21 20:53:23 +0300

    feat: add random planet

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -6,53 +6,76 @@ export const RandomPlanet = () => {
     const swapi = new SwapiService();
     const [state, setState] = useState({});
     const [loader, setLoader] = useState(true);
+    const [error, setError] = useState(false);
 
     const onPlanetLoaded = (planet) => {
         setState(planet);
+        setTimeout(() => {
+            setLoader(false);
+        },2000);
+     
+    }
+    const onError = (error) => {
+        setError(true);
         setLoader(false);
     }
     const updatePlanet = () => {
         const id = Math.floor(Math.random() * 25 + 2);
-        swapi.getPlanet(id).then(planet => onPlanetLoaded(planet));
+        swapi.getPlanet(id).then(planet => onPlanetLoaded(planet))
+        .catch(error => onError(error))
     };
 
     useEffect(() => {
         updatePlanet();
     }, []);
+  
+    const hasdate = !(loader || error);
+    const err = error ? <ErrorIndicator/>:null;
+    const load = loader ? <Spinner/>:null;
+    const content = hasdate? <PlanetView planet={state}/>:null;
 
-    const { id = 2, name, population, rotationPeriod, diameter } = state;
-    if (loader) {
-        return <div className="planet-details">
-            <Spinner />
-        </div>;
-    } else
         return (
-            <div className="planet-details">
-                <div className="planet-details__column">
-                    <div className="planet-details__image">
-                        <img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
-                    </div>
-                </div>
-                <div className="planet-details__column">
-                    <div className="planet-details__column-content">
-                        <div className="planet-details__text">
-                            <h1>{name}</h1>
-                        </div>
-                        <div className="planet-details__text">
-                            <ul className="planet-details__ul">
-                                <li className="planet-details__li"><div className="mark"></div> population: {population}</li>
-                                <li className="planet-details__li"><div className="mark"></div> rotation period: {rotationPeriod}</li>
-                                <li className="planet-details__li"><div className="mark"></div> diameter: {diameter}</li>
-                                <li className="planet-details__li"><div className="mark"></div> Rotation period: 23</li>
-                                <li className="planet-details__li"><div className="mark"></div> Rotation period: 23</li>
-                                <li className="planet-details__li"><div className="mark"></div> Rotation period: 23</li>
-                                <li className="planet-details__li"><div className="mark"></div> Rotation period: 23</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <>
+             {err}
+             {load}
+             {content}
+            </>
 
         )
 }
-
+const ErrorIndicator = () => {
+    return (
+        <div className="planet-details">
+        <h1>Error 404</h1>
+        </div>
+    )
+}
+const PlanetView = ({ planet:{id = 2, name, population, rotationPeriod, diameter} }) => {
+    return (
+        <div className="planet-details">
+        <div className="planet-details__column">
+            <div className="planet-details__image">
+                <img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+            </div>
+        </div>
+        <div className="planet-details__column">
+            <div className="planet-details__column-content">
+                <div className="planet-details__text">
+                    <h1>{name}</h1>
+                </div>
+                <div className="planet-details__text">
+                    <ul className="planet-details__ul">
+                        <li className="planet-details__li"><div className="mark"></div> population: {population}</li>
+                        <li className="planet-details__li"><div className="mark"></div> rotation period: {rotationPeriod}</li>
+                        <li className="planet-details__li"><div className="mark"></div> diameter: {diameter}</li>
+                        <li className="planet-details__li"><div className="mark"></div> Rotation period: 23</li>
+                        <li className="planet-details__li"><div className="mark"></div> Rotation period: 23</li>
+                        <li className="planet-details__li"><div className="mark"></div> Rotation period: 23</li>
+                        <li className="planet-details__li"><div className="mark"></div> Rotation period: 23</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+}
--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -3,6 +3,8 @@ import './Spinner.scss';
 
 export const Spinner = () => {
     return (
+        <div className="planet-details">
         <div class="lds-dual-ring"></div>
+        </div>
     )
 }
